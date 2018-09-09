616450f52aa33fc354a2023bbc6dc1ea4d342986
Anton Veldanov <anton.veldanov@gmail.com>
2018-09-09 01:33:10 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.get('/',(req,res)=>{
+  res.send('WORKS');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+  console.log(`Server started on port ${port}`);
+});
