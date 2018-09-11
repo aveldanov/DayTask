8dc933f85c72dc5d82213e2e9f9286f614366355
Anton Veldanov <anton.veldanov@gmail.com>
2018-09-11 16:50:36 -0700

    forms

---
+++ models/Task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+
+const TaskSchema = new Schema({
+  body:{
+    type:String,
+    required:true
+  },
+  completed:{
+    type: Boolean,
+    default: false
+  },
+  user:{
+    type: Schema.Types.ObjectId,
+    ref:'users'
+  },
+  date:{
+    type:Date,
+    default: Date.now
+  }
+});
+
+//Create collection and add schema and create collection 
+mongoose.model('tasks',TaskSchema);
