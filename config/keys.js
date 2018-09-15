965518cae10220aae73b739013c8ca916146b988
Anton Veldanov <anton.veldanov@gmail.com>
2018-09-14 17:48:36 -0700

    version 1.0.1alpha

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://anton:antonanton1@ds113700.mlab.com:13700/daytask-dev',
-  googleClientID:'242020895903-al3n9osgljvh7p5qnaj6bodajg0t0pqt.apps.googleusercontent.com',
-  googleClientSecret: 'lureI-5FttJSWu6UWkdNzfkv'
+if(process.env.NODE_ENV === 'prod'){
+  module.exports = require('./keys_prod');
+}else{
+  module.exports = require('./keys_dev');
 }
