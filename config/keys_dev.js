6ba6c3ea26de8335cd2bf7484559ce6f0f9663f3
Veldanov <anton.veldanov@gmail.com>
2019-02-08 16:29:50 -0800

    Feb 2019

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://anton:antonanton1@ds113700.mlab.com:13700/daytask-dev',
+  googleClientID: '242020895903-al3n9osgljvh7p5qnaj6bodajg0t0pqt.apps.googleusercontent.com',
+  googleClientSecret: 'lureI-5FttJSWu6UWkdNzfkv'
+}
